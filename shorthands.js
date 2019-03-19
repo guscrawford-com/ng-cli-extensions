bbd71c1664ac60ff70af61ad2b73c493590b79f9
Gus Crawford <gcrawford@sscinc.com>
2019-03-19 19:15:53 -0400

    move shorthands

---
+++ shorthands.js
@@ -0,0 +1,15 @@
+module.exports = {
+    2:{
+        i:'in',
+        at:'in',
+        r:'remove',
+        g:'generate',
+        gen:'generate'
+    },
+    3: {
+        l:'library',
+        lib:'library',
+        ls:'library-scripts',
+        'l-s':'library-scrtips'
+    }
+}
